c8a915c2552694ac564ec480a06e08b4e4751896
zhuzirui <zhuzr3210@163.com>
2025-10-25 11:18:06 +0800

    feat: 测试InitializingBean, DisposableBean

--- spring-test/src/main/java/org/springframework/demos/application/User.java
+++ spring-test/src/main/java/org/springframework/demos/application/User.java
@@ -1,10 +1,22 @@
 package org.springframework.demos.application;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class User {
+public class User implements InitializingBean, DisposableBean {
 	public void sayHello() {
 		System.out.println("Hello, I'm User");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("User is destroyed");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("User is after PropertiesSet");
 	}
 }
